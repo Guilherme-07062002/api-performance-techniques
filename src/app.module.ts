5cc629f35d3838cc8fb32ef41426a0b6905b25e9
Guilherme-07062002 <guilherme762002@gmail.com>
2024-08-31 16:58:46 -0300

    Remove unused code and dependencies in audio module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BullModule } from '@nestjs/bull';
 import { AudioModule } from './audio/audio.module';
 
 @Module({
   imports: [
-    BullModule.forRoot({
-      redis: {
-        host: 'localhost',
-        port: 6379,
-      },
-    }),
     AudioModule
   ],
   controllers: [AppController],
