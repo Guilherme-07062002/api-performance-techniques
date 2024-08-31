8981b12c3168a83db373c571a9d731ce9818f273
Guilherme-07062002 <guilherme762002@gmail.com>
2024-08-31 16:41:34 -0300

    refactor: audio service

--- src/audio/audio.service.ts
+++ src/audio/audio.service.ts
@@ -5,31 +5,30 @@ import * as path from 'path';
 @Injectable()
 export class AudioService {
     async processAudio() {
-        // Caminho para o script que será executado na thread secundária
-        const scriptPath = path.join(__dirname, '../audio/audio.worker.js');
+        const scriptPath = path.resolve(__dirname, '../audio/audio.worker.js');
 
-        // Cria uma nova thread para o processamento
         const worker = new Worker(scriptPath, {
-            workerData: { data: 'processing audio' }, // Dados que serão passados para o worker
+            workerData: { data: 'processing audio' },
         });
 
-        // Manipula a saída da thread
-        worker.on('message', (message) => {
-            console.log('Received from worker:', message);
-        });
+        worker.on('message', this.handleWorkerMessage);
+        worker.on('error', this.handleWorkerError);
+        worker.on('exit', this.handleWorkerExit);
+    }
 
-        // Manipula erros da thread
-        worker.on('error', (error) => {
-            console.error('Error in worker:', error);
-        });
+    private handleWorkerMessage(message: any) {
+        console.log('Received from worker:', message);
+    }
 
-        // Manipula a saída da thread
-        worker.on('exit', (code) => {
-            if (code !== 0) {
-                console.error(`Worker stopped with exit code ${code}`);
-            } else {
-                console.log('Worker exited successfully');
-            }
-        });
+    private handleWorkerError(error: Error) {
+        console.error('Error in worker:', error);
+    }
+
+    private handleWorkerExit(code: number) {
+        if (code !== 0) {
+            console.error(`Worker stopped with exit code ${code}`);
+        } else {
+            console.log('Worker exited successfully');
+        }
     }
 }
